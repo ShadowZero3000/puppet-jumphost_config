a36e13675b8cbd30cca90f06e551042328f0bb63
Josh Souza <git@pureinsomnia.com>
2016-03-28 15:21:27 -0600

    Typos are awesome...

--- jumphost.pp
+++ jumphost.pp
@@ -1,4 +1,4 @@
-ssh_authorized_key { 'Dominance'':
+ssh_authorized_key { 'Dominance':
   user => 'sparrow',
   type => 'ssh-rsa',
   key  => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAsp1454g8s0o2QaCvpWL0ERDnWEdGl7QL25q1cwk0VJza+PEyGewk0O/PrWiUOSUPWMntpg63SiUA82Dc9Tc5yIeMbvdejmNQch/zQo5lj63sVjoM2pJmFqan9I6QUD8N78KvdI8Vyv9+22mTLT5B3eoKNM0fcDfqDixsNl7ATXhirFMBg0ZYfQhNLiOY9UCy+J/oMKrB8m5dk7o0o0ZhL1nGRCnC1nv/1xFCP0xQvOBInJaWG49nO+aOyVMHGFc15xIxbDbKOPrYPNHBgXCz1dVzJ6z/gXaHZRuKcg+wRVaKRrTxrvk/WrWF3uVHj/h8MT9oemVrLqXYPi5IrTzXGw==',
