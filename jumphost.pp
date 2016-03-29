917e19014b10cd67f7798b5a03c522c2cefdedeb
Josh Souza <jsouza@prokarma.com>
2016-03-29 13:37:23 -0600

    Adding work key

--- jumphost.pp
+++ jumphost.pp
@@ -3,3 +3,8 @@ ssh_authorized_key { 'Dominance':
   type => 'ssh-rsa',
   key  => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAsp1454g8s0o2QaCvpWL0ERDnWEdGl7QL25q1cwk0VJza+PEyGewk0O/PrWiUOSUPWMntpg63SiUA82Dc9Tc5yIeMbvdejmNQch/zQo5lj63sVjoM2pJmFqan9I6QUD8N78KvdI8Vyv9+22mTLT5B3eoKNM0fcDfqDixsNl7ATXhirFMBg0ZYfQhNLiOY9UCy+J/oMKrB8m5dk7o0o0ZhL1nGRCnC1nv/1xFCP0xQvOBInJaWG49nO+aOyVMHGFc15xIxbDbKOPrYPNHBgXCz1dVzJ6z/gXaHZRuKcg+wRVaKRrTxrvk/WrWF3uVHj/h8MT9oemVrLqXYPi5IrTzXGw==',
 }
+ssh_authorized_key { 'Work':
+  user => 'sparrow',
+  type => 'ssh-rsa',
+  key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDi3mKV8GmgLynGf4mpAj7lcGPr7Li9KbnbHV7D+l1BX96x+W9c9NvUV/V5AFZIZhGmYf+JWJ7RA7W1VHS0hOgxSlC9pktP3bFuBV5uidKeHK1eREiBADnMD2zNLopFYQ/c5CUHK7qjLQInFgX4CvVSvoc3kJbNdyseqUm6amsC4YqRqSEpS05eavaLY/bpBUr7HB3Qz+2/3101xWwDOSUeOlBdJE7qooP9tvLXxFhgXHndMnG7aSlGX21XyT8eHFAXhMvh/5DEwcAD0a7jalSGRRJzlnI4xtJWOVLnJKAcrSfxuFfSXfpstmYn8jHT7FYFxPk7vC/Tle26rMjg8HMl',
+}
